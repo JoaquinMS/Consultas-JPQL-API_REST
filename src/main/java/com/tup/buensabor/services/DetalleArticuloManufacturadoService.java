e5a6ebf1999d7fbe26c54beb5ecc63bfc73da513
JoaquinMS <118018407+JoaquinMS@users.noreply.github.com>
2023-10-22 14:37:44 -0300

    Commit Joaquin Servicios completos

---
+++ src/main/java/com/tup/buensabor/services/DetalleArticuloManufacturadoService.java
@@ -0,0 +1,20 @@
+package com.tup.buensabor.services;
+
+import com.tup.buensabor.entities.Base;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface DetalleArticuloManufacturadoService <E extends Base,ID extends Serializable>{
+
+    public List<E> findALL() throws Exception;
+
+    public E findById(ID id) throws Exception;
+
+    public E save(E entity) throws Exception;
+
+    public E update(ID id, E entity) throws Exception;
+
+    public boolean delete(ID id) throws Exception;
+
+}
