28afac44572dfbd6293cd87ae928d406868a2ded
Ivan_Molina_14 <ivan14molina@gmail.com>
2023-10-23 09:39:00 -0300

    Commit "@Service" pendiente en la implementacion de DetallePedido y Domicilio

--- src/main/java/com/tup/buensabor/services/DetallePedidoServiceImpl.java
+++ src/main/java/com/tup/buensabor/services/DetallePedidoServiceImpl.java
@@ -6,12 +6,13 @@ import com.tup.buensabor.repositories.BaseRepository;
 import com.tup.buensabor.repositories.DetallePedidoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.events.Event;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
-
+@Service
     public class DetallePedidoServiceImpl extends BaseServiceImpl<DetallePedido, Long> implements DetallePedidoService{
 
         @Autowired
