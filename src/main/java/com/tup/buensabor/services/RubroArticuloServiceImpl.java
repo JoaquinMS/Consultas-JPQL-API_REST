7d426545d6febef3c65ed92125bcbcf59884357e
JoaquinMS <molesjoaquin13@gmail.com>
2023-10-25 00:55:09 -0300

    Commit

--- src/main/java/com/tup/buensabor/services/RubroArticuloServiceImpl.java
+++ src/main/java/com/tup/buensabor/services/RubroArticuloServiceImpl.java
@@ -1,20 +1,17 @@
 package com.tup.buensabor.services;
 
-import com.tup.buensabor.entities.DetallePedido;
-import com.tup.buensabor.entities.Domicilio;
-import com.tup.buensabor.entities.Pedido;
 import com.tup.buensabor.entities.RubroArticulo;
 import com.tup.buensabor.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RubroArticuloImpl extends BaseServiceImpl<RubroArticulo, Long> implements RubroArticuloService{
+public class RubroArticuloServiceImpl extends BaseServiceImpl<RubroArticulo, Long> implements RubroArticuloService{
     @Autowired
     private RubroArticuloRepository rubroarticuloRepository;
 
 
-    public RubroArticuloImpl(BaseRepository<RubroArticulo, Long> baseRepository, RubroArticuloRepository rubroarticuloRepository) {
+    public RubroArticuloServiceImpl(BaseRepository<RubroArticulo, Long> baseRepository, RubroArticuloRepository rubroarticuloRepository) {
         super(baseRepository);
         this.rubroarticuloRepository = rubroarticuloRepository;
     }
