e218a176dff377b99aa0f09e021a88ee348b124b
agustinlongarini <84213966+agustinlongarini@users.noreply.github.com>
2023-10-25 11:21:29 -0300

    ultimos controladores

---
+++ src/main/java/com/tup/buensabor/controllers/RubroArticuloController.java
@@ -0,0 +1,13 @@
+package com.tup.buensabor.controllers;
+
+import com.tup.buensabor.entities.RubroArticulo;
+import com.tup.buensabor.services.RubroArticuloServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "/rubrosarticulo")
+public class RubroArticuloController extends BaseControllerImpl<RubroArticulo, RubroArticuloServiceImpl>{
+}
